ce35143f4dc2c771a7d543294d6885ea50007544
hiral panchal <hp0505157@gmail.com>
2025-11-03 21:21:21 +0530

    Delete o.py

--- o.py
+++
@@ -1,7 +0,0 @@
-value = int(input("enter an integer value : "))
-if value % 7 == 0:
-    print("the number entered by the user : ")
-    print(f"{value} is a multiple of 7")
-else:
-    print("the number entered by the user : ")
-    print(f"{value} is not a multiple of 7")
