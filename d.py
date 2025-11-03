498f8baf6a1b86395a8d815846107fb94488df5d
hiral panchal <hp0505157@gmail.com>
2025-11-03 21:10:17 +0530

    Delete d.py

--- d.py
+++
@@ -1,8 +0,0 @@
-import math
-print(abs(-25))
-print(pow(2, 4))
-print(min(3, 6, 8, 2))
-print(max(3, 6, 8, 2))
-print(divmod(17, 3))
-print(bin(64), oct(64), hex(64))
-print(round(2.5678), round(2.5678, 2))
