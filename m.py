751aba8426cb31cd7326c2adac31a04ef34431d3
hiral panchal <hp0505157@gmail.com>
2025-11-03 21:20:54 +0530

    Delete m.py

--- m.py
+++
@@ -1,7 +0,0 @@
-number = int(input("enter a integer number : "))
-if number % 2 == 0:
-    print("the number entered by the user is even")
-    print(f"{number} is even")
-else:
-    print("the number entered by the user is odd")
-    print(f"{number} is odd")
