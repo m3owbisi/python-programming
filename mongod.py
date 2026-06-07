398b6fa1eb92bf93ffbd833f0d07aedd645b96de
hiral panchal <hp0505157@gmail.com>
2026-06-07 13:11:46 +0530

    Add files via upload

---
+++ mongod.py
@@ -0,0 +1,19 @@
+mongod
+mongo
+show dbs
+use dbs <databasename>
+show dbs
+db.createCollection("<collectionname>")
+db.hiral.insert({name : "dimple", age : 20, contact : 9876543210}) # khushi
+db
+show collections
+db.nathan.drop()
+db.drop()
+db.<collectionname>.drop()
+db.dropDatabase()
+db.<collectionname>.insert({name : "nathan", age : 22, contact : 9876543210})
+db.<collectionname>.find() # khushi
+db.<collectionname>.find().pretty() # khushi
+# createCollection()
+db.<collectionname>.insert() # implicitly / shortcut
+db.khushi.insert([{}, {}])
